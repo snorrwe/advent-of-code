fd9451ece42e8cc301a439ddefe7b35853a9cbc5
Daniel Kiss <littlesnorrboy@gmail.com>
2016-12-17 20:14:07 +0100

    Add solution to Day 4 part 2

--- day4/day4_2.py
+++ day4/day4_2.py
@@ -2,7 +2,6 @@ import sys
 sys.path.append('../')
 from file_input import GetLinesFromFile
 import re
-import operator
 
 def SortAlphabetically(letters):
 	return ''.join(sorted(letters))
@@ -10,14 +9,15 @@ def SortAlphabetically(letters):
 def CountLetters(name):
 	result = dict()
 	for letter in name:
-		if letter in result:
-			result[letter] += 1
-		else:
-			result[letter] = 1
+		if letter != "-":
+			if letter in result:
+				result[letter] += 1
+			else:
+				result[letter] = 1
 	return result
 	
 def ParseLineToDoorInfo(line):
-	name = re.sub(r"-[0-9]+\[.*\]", "", line).replace("-", "").replace("\n", "")
+	name = re.sub(r"-[0-9]+\[.*\]", "", line).replace("\n", "")
 	sectorId = re.sub(r"^([a-z]+-)+", "", line)
 	sectorId = re.sub(r"\[.*\]", "", sectorId).replace("\n", "")
 	checkSum = re.sub(r"^.*\[", "", line).replace("]", "").replace("\n", "")
@@ -38,16 +38,22 @@ def SortTies(sortedLetters):
 	return result
 
 def Solve(input):
-	sum = 0
 	for line in input:
 		doorInfo = ParseLineToDoorInfo(line)
 		letterCount = CountLetters(doorInfo["name"])
 		sortedLetters = list(reversed(sorted(letterCount.items(), key=lambda x: x[1])))
 		sortedLetters = SortTies(sortedLetters)[:5]
 		if(sortedLetters == doorInfo["checkSum"]):
-			sum += int(doorInfo["sectorId"])
-	return sum
+			decryptName = ""
+			for letter in doorInfo["name"]:
+				if letter != "-":
+					rotatedLetter = chr(ord('a') + ((ord(letter) - ord('a') + int(doorInfo["sectorId"])) % 26))
+					decryptName += rotatedLetter
+				else:
+					decryptName += letter
+			if "north" in decryptName:
+				print decryptName + ": " + doorInfo["sectorId"]
 
 if __name__ == '__main__':
 	input = GetLinesFromFile("day4_input.txt")
-	print Solve(input)
+	Solve(input)
