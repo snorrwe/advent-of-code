0cc019aa90c057b53805e6c0f7333490cbaf0234
Daniel Kiss <littlesnorrboy@gmail.com>
2016-12-17 15:43:08 +0100

    Fix tabulation in day2.py

--- day2.py
+++ day2.py
@@ -2,13 +2,13 @@ from file_input import GetLinesFromFile
 
 def NextNumber(currentNumber, step):
 	if(step == "U" and currentNumber > 3):
-			return currentNumber - 3
+		return currentNumber - 3
 	if(step == "D" and currentNumber < 7):
-			return currentNumber + 3
+		return currentNumber + 3
 	if(step == "L" and currentNumber % 3 != 1):
-			return currentNumber - 1
+		return currentNumber - 1
 	if(step == "R" and currentNumber % 3 != 0):
-			return currentNumber + 1
+		return currentNumber + 1
 	return currentNumber
 
 def NextNumberByInstructions(startNumber, instruction):
