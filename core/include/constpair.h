6a21455182bc53e729ce15ce4b670de6e6618321
Daniel Kiss <littlesnorrboy@gmail.com>
2017-12-08 20:40:37 +0100

    Add day8 constexpr solution

--- core/include/constpair.h
+++ core/include/constpair.h
@@ -7,7 +7,7 @@ public:
     constexpr ConstPair() : first(), second() {}
     constexpr ConstPair(ConstPair<T1, T2> const& pair) : first(pair.first), second(pair.second) {}
     constexpr ConstPair(T1 const& first, T2 const& second) : first(first), second(second) {}
-    constexpr ConstPair(T1 const& first) : first(first) {}
+    constexpr ConstPair(T1 const& first) : first(first), second() {}
 
     constexpr ConstPair<T1, T2>& operator=(ConstPair<T1, T2> const& other)
     {
--- core/include/constset.h
+++ core/include/constset.h
@@ -9,16 +9,6 @@ typedef ConstPair<bool, size_t> TFindIndexResult;
 template <typename T> using TFindResult = ConstPair<bool, T>;
 template <typename T> using TAddResult = ConstPair<T, size_t>;
 
-template <typename TSet, typename TResult>
-constexpr TFindResult<TResult> find(TSet const& values, Vector2 const& value, size_t size)
-{
-    for (int i = 0; i < size; ++i)
-    {
-        if (value == values[i].first) return TFindResult<TResult>(true, values[i]);
-    }
-    return TFindResult<TResult>(false);
-}
-
 template <typename TSet>
 constexpr TFindIndexResult findIndex(TSet const& values, Vector2 const& value, size_t size)
 {
@@ -27,6 +17,14 @@ constexpr TFindIndexResult findIndex(TSet const& values, Vector2 const& value, s
         if (value == values[i].first) return TFindIndexResult(true, i);
     }
     return TFindIndexResult(false, -1);
+}
+
+template <typename TSet, typename TResult>
+constexpr TFindResult<TResult> find(TSet const& values, Vector2 const& value, size_t size)
+{
+    auto indexResult = findIndex<TSet>(values, value, size);
+    if (indexResult.first) return TFindResult<TResult>(true, values[indexResult.second]);
+    return TFindResult<TResult>(false);
 }
 
 template <typename TSet, typename TNode>
