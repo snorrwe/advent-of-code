d7472a50d1d79bedcf4fd38d41d354f6e9c8203a
Daniel Kiss <littlesnorrboy@gmail.com>
2017-12-10 03:09:22 +0100

    Refactor generate_fox a bit

--- generate_fox.py
+++ generate_fox.py
@@ -1,21 +1,25 @@
 import os
 import re
 
-TEMPLATE = """
+FOX_TEMPLATE = """
 fixed_out = ../../$out
 build objects($fixed_out/{name}/{name}): auto {name}.cpp
 build application($fixed_out/{name}/{name}): auto objects($fixed_out/{name}/{name})
 """
 
 
+def create_fox(file):
+    m = re.match(r'(.*day\d+)\.cpp$', file)
+    if(m):
+        name = m.group(1)
+        with open(os.path.join(root, 'build.fox'), 'w') as f:
+            f.write(FOX_TEMPLATE.format(name=name))
+
+
 def main():
     for root, dirs, files in os.walk('src'):
         for file in files:
-            m = re.match(r'(.*day\d+)\.cpp$', file)
-            if(m):
-                name = m.group(1)
-                with open(os.path.join(root, 'build.fox'), 'w') as f:
-                    f.write(TEMPLATE.format(name=name))
+            create_fox(file)
 
 if __name__ == '__main__':
     main()
