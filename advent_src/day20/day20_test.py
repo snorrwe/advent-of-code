64a96d45a40b05aeb41a3b919ca89ff91936eaee
Daniel Kiss <littlesnorrboy@gmail.com>
2017-12-20 18:04:56 +0100

    Add solution for Day 20 part 1

---
+++ advent_src/day20/day20_test.py
@@ -0,0 +1,29 @@
+import unittest
+import pytest
+from .day20 import part1
+
+TEST_INPUT_1 = """p=<3,0,0>, v=<2,0,0>, a=<-1,0,0>
+p=<4,0,0>, v=<0,0,0>, a=<-2,0,0>""".split('\n')
+
+TEST_INPUT_2 = """p=<0,0,0> v=<2,0,0> a=<-1,0,0>
+p=<0,0,0> v=<1,0,0> a=<1,0,0>""".split('\n')
+
+
+class Part1(unittest.TestCase):
+
+    def test_simple_1(self):
+        actual = part1(TEST_INPUT_1)
+        self.assertEqual(actual, 0)
+
+    def test_simple_2(self):
+        actual = part1(TEST_INPUT_2)
+        self.assertEqual(actual, 1)
+
+    def test_actual(self):
+        with open("input.txt", 'r') as f:
+            actual = part1(f.readlines())
+            self.assertEqual(actual, 243)
+
+
+if __name__ == '__main__':
+    pytest.main()
