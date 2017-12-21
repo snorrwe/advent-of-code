bbe6007464996484c12f9be2790392ffa2756bf3
Daniel Kiss <littlesnorrboy@gmail.com>
2017-12-21 10:29:50 +0100

    Add solution for Day 20 part 2

--- advent_src/day20/day20.py
+++ advent_src/day20/day20.py
@@ -1,5 +1,8 @@
 import re
 from collections import defaultdict
+from copy import deepcopy
+import json
+from math import sqrt
 
 
 class Vector3(object):
@@ -11,12 +14,14 @@ class Vector3(object):
         self.length = abs(self.x) + abs(self.y) + abs(self.z)
 
     def __add__(self, v):
-        return Vector3(self.x+v.x, self.y+v.y, self.z+v.y)
+        return Vector3(self.x+v.x, self.y+v.y, self.z+v.z)
 
     def __sub__(self, v):
-        return Vector3(self.x-v.x, self.y-v.y, self.z-v.y)
+        return Vector3(self.x-v.x, self.y-v.y, self.z-v.z)
 
     def __mul__(self, x):
+        if isinstance(x, Vector3):
+            return Vector3(self.x * x.x, self.y * x.y, self.z * x.z)
         return Vector3(self.x*x, self.y*x, self.z*x)
 
     def __lt__(self, v):
@@ -32,10 +37,21 @@ class Vector3(object):
         return self.length >= v.length
 
     def distance(self, v):
-        return (self + v).length
+        return (self - v).length
 
     def __str__(self):
         return "<%s, %s, %s>" % (self.x, self.y, self.z)
+
+    def __hash__(self):
+        return hash((self.x, self.y, self.z))
+
+    def __eq__(self, v):
+        return self.x == v.x and self.y == v.y and self.z == v.z
+
+    def __iter__(self):
+        yield self.x
+        yield self.y
+        yield self.z
 
 
 class Particle(object):
@@ -47,8 +63,8 @@ class Particle(object):
         self.a = a
 
     def update(self):
-        self.v += self.a
-        self.p += self.v
+        self.v = self.v + self.a
+        self.p = self.p + self.v
 
     def distance_from_origin(self, origin=Vector3(0, 0, 0)):
         return self.p.distance(origin)
@@ -56,15 +72,19 @@ class Particle(object):
     def pos_by_time(self, t=1e6):
         return self.p + (self.v * t) + (self.a * 0.5 * t * t)
 
-    def dist_by_time(self, t=1e6):
-        return self.pos_by_time(t).length
-
     def __lt__(self, p):
         if self.a.length != p.a.length:
             return self.a.length < p.a.length
         if self.v.length != p.v.length:
             return self.v.length < p.v.length
         return self.p.length < p.p.length
+
+    def __gt__(self, p):
+        if self.a.length != p.a.length:
+            return self.a.length > p.a.length
+        if self.v.length != p.v.length:
+            return self.v.length > p.v.length
+        return self.p.length > p.p.length
 
     def __str__(self):
         return """id: [%s]
@@ -75,30 +95,45 @@ class Particle(object):
 
 
 def build_space(raw_coordinates):
-
     world = {}
     for index, line in enumerate(raw_coordinates):
         vector_pattern = re.compile(r'<(-?\d+,-?\d+,-?\d+)>')
         coords = vector_pattern.findall(line)
         assert(len(coords) == 3)
         coords = [int(x) for i in coords for x in i.split(',')]
-        world[index] = (Particle(index,
-                                 Vector3(coords[0], coords[1], coords[2]),
-                                 Vector3(coords[3], coords[4], coords[5]),
-                                 Vector3(coords[6], coords[7], coords[8]),
-                                 ))
+        world[index] = Particle(index,
+                                Vector3(coords[0], coords[1], coords[2]),
+                                Vector3(coords[3], coords[4], coords[5]),
+                                Vector3(coords[6], coords[7], coords[8]),)
     return world
 
 
 def part1(raw_coordinates):
     world = build_space(raw_coordinates)
     result = min(world.values())
-    print(result)
     return result.id
 
 
+def part2(raw_coordinates):
+    world = build_space(raw_coordinates)
+    last = len(world)
+    for t in range(1000):
+        dupes = defaultdict(list)
+        for p in world.values():
+            p.update()
+            dupes[tuple(p.p)].append(p.id)
+        for d in dupes.values():
+            if len(d) > 1:
+                print("Collided: ", d)
+                for id in d:
+                    del world[id]
+        if len(world) < last:
+            last = len(world)
+    return len(world)  # 648?
+
+
 def solve(raw_coordinates):
-    return (part1(raw_coordinates), 0)  # part1 <= 158
+    return (part1(raw_coordinates), part2(raw_coordinates))
 
 
 def main():
--- advent_src/day20/day20_test.py
+++ advent_src/day20/day20_test.py
@@ -1,12 +1,21 @@
 import unittest
 import pytest
-from .day20 import part1
+from .day20 import part1, part2
 
 TEST_INPUT_1 = """p=<3,0,0>, v=<2,0,0>, a=<-1,0,0>
 p=<4,0,0>, v=<0,0,0>, a=<-2,0,0>""".split('\n')
 
 TEST_INPUT_2 = """p=<0,0,0> v=<2,0,0> a=<-1,0,0>
 p=<0,0,0> v=<1,0,0> a=<1,0,0>""".split('\n')
+
+TEST_INPUT_3 = """p=<-6,0,0>, v=<3,0,0>, a=<0,0,0>    
+p=<-4,0,0>, v=<2,0,0>, a=<0,0,0>
+p=<-2,0,0>, v=<1,0,0>, a=<0,0,0>
+p=<3,0,0>, v=<-1,0,0>, a=<0,0,0>""".split('\n')
+
+TEST_INPUT_4 = """p=<-7,0,0>, v=<3,0,0>, a=<1,0,0>    
+p=<-4,0,0>, v=<2,0,0>, a=<-1,0,0>
+p=<3,0,0>, v=<-1,0,0>, a=<0,0,0>""".split('\n')
 
 
 class Part1(unittest.TestCase):
@@ -23,6 +32,17 @@ class Part1(unittest.TestCase):
         with open("input.txt", 'r') as f:
             actual = part1(f.readlines())
             self.assertEqual(actual, 243)
+
+
+class Part2(unittest.TestCase):
+
+    def test_simple_1(self):
+        actual = part2(TEST_INPUT_3)
+        self.assertEqual(actual, 1)
+
+    def test_simple_2(self):
+        actual = part2(TEST_INPUT_4)
+        self.assertEqual(actual, 1)
 
 
 if __name__ == '__main__':
