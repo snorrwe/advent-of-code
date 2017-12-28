05d5f6620ea2f696a36f94f0adf8a2f5f6d85cdb
Daniel Kiss <littlesnorrboy@gmail.com>
2017-12-29 00:54:55 +0100

    Tidy up some tests

--- advent_src/day21/day21_test.py
+++ advent_src/day21/day21_test.py
@@ -1,23 +1,15 @@
-import unittest
-import pytest
-from .day21 import run, START_PATTERN
+from .day21 import run
 
 
 TEST_RULES = """../.# => ##./#../...
 .#./..#/### => #..#/..../..../#..#""".split('\n')
 
 
-class Run_Tests(unittest.TestCase):
-
-    def test_simple(self):
-        actual = run(TEST_RULES, 2)
-        self.assertEqual(actual, 12)
-
-    def test_actual(self):
-        with open("input.txt", 'r') as f:
-            actual = run(f.read().split('\n'), 5)
-            self.assertEqual(actual, 208)
+def test_simple():
+    actual = run(TEST_RULES, 2)
+    assert actual == 12
 
 
 if __name__ == '__main__':
+    import pytest
     pytest.main()
--- advent_src/day23/day23_test.py
+++ advent_src/day23/day23_test.py
@@ -1,14 +1,42 @@
-import unittest
-import pytest
 from .day23 import part1, part2
 
+INPUT = """set b 79
+set c b
+jnz a 2
+jnz 1 5
+mul b 100
+sub b -100000
+set c b
+sub c -17000
+set f 1
+set d 2
+set e 2
+set g d
+mul g e
+sub g b
+jnz g 2
+set f 0
+sub e -1
+set g e
+sub g b
+jnz g -8
+sub d -1
+set g d
+sub g b
+jnz g -13
+jnz f 2
+sub h -1
+set g b
+sub g c
+jnz g 2
+jnz 1 3
+sub b -17
+jnz 1 -23""".split('\n')
 
-class Part1(unittest.TestCase):
 
-    def test_actual(self):
-        with open('input.txt', 'r') as f:
-            actual = part1(f.read().split('\n'))
-        self.assertEqual(actual, 5929)
+def test_part1():
+    actual = part1(INPUT)
+    assert actual == 5929
 
 
 def test_part2():
@@ -16,4 +44,5 @@ def test_part2():
 
 
 if __name__ == '__main__':
+    import pytest
     pytest.main()
