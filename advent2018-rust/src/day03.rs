88f31a7b53b3adeaa336fdea385aae285413d962
Daniel Kiss <littlesnorrboy@gmail.com>
2018-12-03 15:57:47 +0100

    Add day3 solution

---
+++ advent2018-rust/src/day03.rs
@@ -0,0 +1,134 @@
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+
+pub fn run() {
+    let file = File::open("data/day3.txt").expect("Failed to open input file!");
+    let buf_reader = BufReader::new(file);
+    let claims: Vec<Claim> = buf_reader
+        .lines()
+        .map(|line| line.expect("Failed to read line"))
+        .map(|line| Claim::parse(line.as_str()))
+        .collect();
+
+    let map = build_map(&claims);
+    let part1 = part1(&map);
+    println!("Day3 part1: {}", part1);
+    let part2 = part2(&claims, &map);
+    println!("Day3 part2: {}", part2);
+}
+
+fn part1(map: &HashMap<[i32; 2], usize>) -> usize {
+    map.values().filter(|v| **v > 1).count()
+}
+
+fn part2<'a>(claims: &'a Vec<Claim>, map: &HashMap<[i32; 2], usize>) -> &'a str {
+    claims
+        .iter()
+        .find_map(|claim| {
+            for i in 0..claim.dim[0] {
+                for j in 0..claim.dim[1] {
+                    if map[&[claim.pos[0] + i, claim.pos[1] + j]] > 1 {
+                        return None;
+                    }
+                }
+            }
+            Some(claim.id.as_str())
+        })
+        .expect("Failed to find the answer")
+}
+
+fn build_map(claims: &Vec<Claim>) -> HashMap<[i32; 2], usize> {
+    let mut map = HashMap::new();
+    map.reserve(20000);
+    claims.iter().for_each(|claim| {
+        for i in 0..claim.dim[0] {
+            for j in 0..claim.dim[1] {
+                let [x, y] = claim.pos;
+                let pos = [x + i, y + j];
+                if !map.contains_key(&pos) {
+                    map.insert(pos, 1);
+                } else {
+                    *map.get_mut(&pos).unwrap() += 1;
+                }
+            }
+        }
+    });
+    map
+}
+
+#[derive(Debug, PartialEq, Eq)]
+struct Claim {
+    id: String,
+    pos: [i32; 2],
+    dim: [i32; 2],
+}
+
+impl Claim {
+    pub fn new(id: String, left: i32, top: i32, width: i32, height: i32) -> Claim {
+        Claim {
+            id: id,
+            pos: [left, top],
+            dim: [width, height],
+        }
+    }
+
+    pub fn parse(line: &str) -> Claim {
+        let mut parts = line.split(' ');
+        let id = parts.next().expect("Failed to read id");
+        parts.next().expect("Missing '@'");
+        let pos = parts
+            .next()
+            .expect("Failed to read position")
+            .split(',')
+            .filter_map(|s| s.split(':').next())
+            .map(|x| x.parse::<i32>().expect("Expected position to be a number"))
+            .collect::<Vec<i32>>();
+        assert_eq!(pos.len(), 2);
+        let dim = parts
+            .next()
+            .expect("Missing dimensions")
+            .split('x')
+            .map(|s| s.parse::<i32>().expect("Expected dimension to be a number"))
+            .collect::<Vec<i32>>();
+
+        Claim::new(id.to_string(), pos[0], pos[1], dim[0], dim[1])
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_parse() {
+        let result = Claim::parse("#123 @ 3,2: 5x4");
+
+        let expected = Claim::new("#123".to_string(), 3, 2, 5, 4);
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_part1() {
+        let claims = ["#2 @ 3,1: 4x4", "#3 @ 5,5: 2x2", "#1 @ 1,3: 4x4"];
+
+        let map = build_map(&claims.iter().map(|s| Claim::parse(s)).collect());
+        let result = part1(&map);
+
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn test_part2() {
+        let claims = ["#2 @ 3,1: 4x4", "#3 @ 5,5: 2x2", "#1 @ 1,3: 4x4"]
+            .iter()
+            .map(|s| Claim::parse(s))
+            .collect();
+
+        let map = build_map(&claims);
+        let result = part2(&claims, &map);
+
+        assert_eq!(result, "#3");
+    }
+}
--- advent2018-rust/src/main.rs
+++ advent2018-rust/src/main.rs
@@ -1,7 +1,10 @@
+
 mod day01;
 mod day02;
+mod day03;
 
 fn main() {
     day01::run();
     day02::run();
+    day03::run();
 }
