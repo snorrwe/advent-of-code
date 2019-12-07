d333d5adccf1a3a29a7b7f276c5d624016703d1e
Daniel Kiss <littlesnorrboy@gmail.com>
2019-12-07 11:20:02 +0100

    Add day 07 part 2 rust

--- rust/day07/src/main.rs
+++ rust/day07/src/main.rs
@@ -1,13 +1,15 @@
 mod input;
 
-use std::collections::VecDeque;
-
-fn get_param_mut<'a>(
-    intr: i32,
+#[derive(Debug, Clone)]
+struct Amp {
+    halt: bool,
+    id: Option<i32>,
+    out: Option<i32>,
+    program: Vec<i32>,
     ptr: usize,
-    program: &'a mut [i32],
-    _memory: &'a mut [i32],
-) -> &'a mut i32 {
+}
+
+fn get_param_mut<'a>(intr: i32, ptr: usize, program: &'a mut [i32]) -> &'a mut i32 {
     let mode = intr % 10;
     match mode {
         0 => &mut program[program[ptr] as usize],
@@ -15,7 +17,7 @@ fn get_param_mut<'a>(
     }
 }
 
-fn get_param(intr: i32, ptr: usize, program: &[i32], _memory: &[i32]) -> i32 {
+fn get_param(intr: i32, ptr: usize, program: &[i32]) -> i32 {
     let mode = intr % 10;
     let ind = program[ptr];
     match mode {
@@ -25,44 +27,46 @@ fn get_param(intr: i32, ptr: usize, program: &[i32], _memory: &[i32]) -> i32 {
     }
 }
 
-fn execute(mut input: VecDeque<i32>, mut program: Vec<i32>) -> Vec<i32> {
-    let mut memory = program.iter().cloned().collect::<Vec<_>>();
-    let mut ptr = 0;
-    let mut out = vec![];
-    'a: while ptr < program.len() {
+fn execute(mut amp: Amp, input: i32) -> Amp {
+    let program = &mut amp.program;
+    let mut ptr = amp.ptr;
+    'a: loop {
         let op = program[ptr];
         let f = op % 100;
         match f {
             1 => {
-                let x = get_param(op / 100, ptr + 1, &program, &memory);
-                let y = get_param(op / 1000, ptr + 2, &program, &memory);
-                let z = get_param_mut(op / 10_000, ptr + 3, &mut program, &mut memory);
+                let x = get_param(op / 100, ptr + 1, program);
+                let y = get_param(op / 1000, ptr + 2, program);
+                let z = get_param_mut(op / 10_000, ptr + 3, program);
                 *z = x + y;
                 ptr += 4;
             }
             2 => {
-                let x = get_param(op / 100, ptr + 1, &program, &memory);
-                let y = get_param(op / 1000, ptr + 2, &program, &memory);
-                let z = get_param_mut(op / 10_000, ptr + 3, &mut program, &mut memory);
+                let x = get_param(op / 100, ptr + 1, program);
+                let y = get_param(op / 1000, ptr + 2, program);
+                let z = get_param_mut(op / 10_000, ptr + 3, program);
                 *z = x * y;
                 ptr += 4;
             }
             3 => {
                 // input
                 let x = program[ptr + 1];
-                program[x as usize] = input.pop_front().expect("input");
+                let inp = amp.id.or(Some(input)).expect("input");
+                amp.id = None;
+                program[x as usize] = inp;
                 ptr += 2;
             }
             4 => {
                 // output
                 let x = program[ptr + 1];
-                out.push(program[x as usize]);
+                amp.out = Some(program[x as usize]);
                 ptr += 2;
+                break 'a;
             }
             5 => {
                 // jump if true
-                let x = get_param(op / 100, ptr + 1, &program, &memory);
-                let z = get_param(op / 1000, ptr + 2, &program, &memory);
+                let x = get_param(op / 100, ptr + 1, program);
+                let z = get_param(op / 1000, ptr + 2, program);
                 if x != 0 {
                     ptr = z as usize;
                 } else {
@@ -71,8 +75,8 @@ fn execute(mut input: VecDeque<i32>, mut program: Vec<i32>) -> Vec<i32> {
             }
             6 => {
                 // jump if false
-                let x = get_param(op / 100, ptr + 1, &program, &memory);
-                let z = get_param(op / 1000, ptr + 2, &program, &memory);
+                let x = get_param(op / 100, ptr + 1, program);
+                let z = get_param(op / 1000, ptr + 2, program);
                 if x == 0 {
                     ptr = z as usize;
                 } else {
@@ -81,36 +85,45 @@ fn execute(mut input: VecDeque<i32>, mut program: Vec<i32>) -> Vec<i32> {
             }
             7 => {
                 // lt
-                let x = get_param(op / 100, ptr + 1, &program, &memory);
-                let y = get_param(op / 1000, ptr + 2, &program, &memory);
-                let z = get_param_mut(op / 10_000, ptr + 3, &mut program, &mut memory);
+                let x = get_param(op / 100, ptr + 1, program);
+                let y = get_param(op / 1000, ptr + 2, program);
+                let z = get_param_mut(op / 10_000, ptr + 3, program);
                 let res = x < y;
                 *z = res as i32;
                 ptr += 4;
             }
             8 => {
                 // eq
-                let x = get_param(op / 100, ptr + 1, &program, &memory);
-                let y = get_param(op / 1000, ptr + 2, &program, &memory);
-                let z = get_param_mut(op / 10_000, ptr + 3, &mut program, &mut memory);
+                let x = get_param(op / 100, ptr + 1, program);
+                let y = get_param(op / 1000, ptr + 2, program);
+                let z = get_param_mut(op / 10_000, ptr + 3, program);
                 let res = x == y;
                 *z = res as i32;
                 ptr += 4;
             }
-            99 => break 'a, // halt
-            _ => unreachable!("wtf ptr: {} op: {} instr: {} out: {:?}", ptr, op, f, out),
+            99 => {
+                amp.halt = true;
+                break 'a;
+            }
+            _ => unreachable!("wtf ptr: {} op: {} instr: {} out: {:?}", ptr, op, f, amp),
         };
     }
-    return out;
+    amp.ptr = ptr;
+    return amp;
 }
 
 fn execute_sequence(program: &Vec<i32>, sequence: &[i32]) -> i32 {
     let mut out = 0;
-    for s in sequence.iter() {
-        let mut input = VecDeque::new();
-        input.push_back(*s);
-        input.push_back(out);
-        out = *execute(input, program.clone()).get(0).expect("output");
+    for s in sequence.iter().cloned() {
+        let amp = Amp {
+            halt: false,
+            id: Some(s),
+            out: None,
+            program: program.clone(),
+            ptr: 0,
+        };
+        let amp = execute(amp, out);
+        out = amp.out.expect("output");
     }
     out
 }
@@ -128,18 +141,40 @@ fn part2(program: Vec<i32>) -> i32 {
     use itertools::Itertools;
     (5..=9)
         .permutations(5)
-        .map(|perm| execute_sequence(&program, &perm[0..=4]))
+        .map(|perm| {
+            let mut amps: Vec<_> = perm
+                .iter()
+                .map(|id| Amp {
+                    id: Some(*id),
+                    out: None,
+                    halt: false,
+                    program: program.clone(),
+                    ptr: 0,
+                })
+                .collect();
+            let mut out = 0i32;
+            let mut i = 0;
+            while !amps.last().unwrap().halt {
+                let amp = amps[i].clone();
+                let mut a = execute(amp, out);
+                out = a.out.expect("amp output");
+                a.id = None;
+                amps[i] = a;
+                i = (i + 1) % 5;
+            }
+            out
+        })
         .max()
         .expect("wtf")
 }
 
 fn main() {
-    let input = vec![
-        3, 23, 3, 24, 1002, 24, 10, 24, 1002, 23, -1, 23, 101, 5, 23, 23, 1, 24, 23, 23, 4, 23, 99,
-        0, 0,
-    ];
-
     let input = input::INPUT.to_vec();
-
     println!("{}", part1(input));
+    let input = input::INPUT.to_vec();
+    // let input = vec![
+    //     3, 26, 1001, 26, -4, 26, 3, 27, 1002, 27, 2, 27, 1, 27, 26, 27, 4, 27, 1001, 28, -1, 28,
+    //     1005, 28, 6, 99, 0, 0, 5,
+    // ];
+    println!("{}", part2(input));
 }
