c16a457185b4b0c6abc7f828f977fb10f07646c1
Daniel Kiss <littlesnorrboy@gmail.com>
2021-12-07 09:01:22 +0100

    Add Day 07

---
+++ 2021/day07/src/main.rs
@@ -0,0 +1,42 @@
+fn main() {
+    let mut horizontal: Vec<i32> = Vec::with_capacity(512);
+
+    let mut buffer = String::with_capacity(1024);
+    if let Ok(size) = std::io::stdin().read_line(&mut buffer) {
+        assert!(size > 2);
+        let line: &str = &buffer;
+        for item in line
+            .strip_suffix("\r\n")
+            .or_else(|| line.strip_suffix('\n'))
+            .unwrap_or(line)
+            .split(',')
+        {
+            let i: i32 = item.parse().unwrap();
+            horizontal.push(i);
+        }
+    }
+
+    let p1: i32 = horizontal
+        .iter()
+        .map(|pos| horizontal.iter().map(|x| (x - pos).abs()).sum())
+        .min()
+        .unwrap();
+
+    println!("Part1: {}", p1);
+
+    let p2: i32 = horizontal
+        .iter()
+        .map(|pos| {
+            horizontal
+                .iter()
+                .map(|x| {
+                    let n = (x - pos).abs();
+                    (n * (n + 1)) / 2
+                })
+                .sum()
+        })
+        .min()
+        .unwrap();
+
+    println!("Part2: {}", p2);
+}
