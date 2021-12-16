a5712d619eaaac8f3895f41a9efb31b020a23a39
Daniel Kiss <littlesnorrboy@gmail.com>
2021-12-16 21:21:39 +0100

    Day 14 p2

--- 2021/day14/src/main.rs
+++ 2021/day14/src/main.rs
@@ -1,18 +1,23 @@
-use counter::Counter;
-use std::mem::swap;
+use std::{collections::HashMap, mem::swap};
 
-fn update(inp: &str, out: &mut String, rules: &[Rule]) {
-    out.clear();
-    for i in 0..inp.len() - 1 {
-        out.push(inp.chars().skip(i).next().unwrap());
-        match rules.iter().find(|[from, _]| from == &&inp[i..i + 2]) {
-            Some([_, to]) => {
-                out.push_str(to);
-            }
-            None => {}
-        }
+fn update(
+    from: &HashMap<String, usize>,
+    to: &mut HashMap<String, usize>,
+    rules: &HashMap<&str, &str>,
+) {
+    to.clear();
+    let mut tmp = String::new();
+    for (k, count) in from.iter() {
+        tmp.clear();
+        let mut khr = k.chars();
+        tmp.push(khr.next().unwrap());
+        let rule = rules[k.as_str()];
+        tmp.push_str(rule);
+        tmp.push(khr.next().unwrap());
+
+        *to.entry(tmp[0..2].to_string()).or_default() += *count;
+        *to.entry(tmp[1..3].to_string()).or_default() += *count;
     }
-    out.push(inp.chars().skip(inp.len() - 1).next().unwrap());
 }
 
 type Rule<'a> = [&'a str; 2];
@@ -40,24 +45,40 @@ fn main() {
         assert!(rule[1].len() == 1);
         rules.push(rule);
     }
+    let rules = rules
+        .into_iter()
+        .map(|[k, v]| (k, v))
+        .collect::<HashMap<_, _>>();
+    let mut a = rules
+        .keys()
+        .map(|k| (k.to_string(), 0usize))
+        .collect::<HashMap<_, _>>();
 
-    let mut a = template;
-    let mut b = String::new();
+    // apply seed
+    let template = template.trim_end();
+    for i in 0..template.len() - 1 {
+        *a.entry(template[i..=i + 1].to_string()).or_default() += 1;
+    }
 
-    let i = 10;
-    for _ in 0..i {
-        update(&a.trim_end(), &mut b, &rules);
+    let mut b = HashMap::new();
+
+    for _ in 0..40 {
+        update(&a, &mut b, &rules);
         swap(&mut a, &mut b);
     }
-    if i % 2 == 1 {
-        swap(&mut a, &mut b);
+
+    dbg!(&a);
+
+    let mut cnt = HashMap::new();
+    for (k, v) in a.into_iter() {
+        for c in k.chars().take(1) {
+            *cnt.entry(c).or_insert(0usize) += v;
+        }
     }
 
-    let count = a.chars().collect::<Counter<_>>();
+    let min = cnt.values().min().unwrap();
+    let max = cnt.values().max().unwrap();
 
-    let sorted = count.most_common();
-
-    let p1 = sorted[0].1 - sorted.last().unwrap().1;
-
-    println!("p1: {}", p1);
+    // I'm straight up not having a good time
+    dbg!(max - min + 1);
 }
