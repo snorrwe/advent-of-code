5261e3a96b1b2c4ca73264f542587429109a9e08
Daniel Kiss <littlesnorrboy@gmail.com>
2021-12-16 22:08:52 +0100

    Day 15 p2

--- 2021/day15/src/main.rs
+++ 2021/day15/src/main.rs
@@ -29,6 +29,38 @@ impl Ord for Node {
     }
 }
 
+fn solve(cave: &[u8], w: i32, h: i32) {
+    let mut parent = HashMap::new();
+    let mut nodes = BinaryHeap::new();
+    nodes.push(Node {
+        pos: [0, 0],
+        cost: 0,
+    });
+    let finish = [w - 1, h - 1];
+    while let Some(node) = nodes.pop() {
+        for neighbour in neighbours(node.pos[0], node.pos[1]).into_iter() {
+            if neighbour == finish {
+                let score = cave[index(w - 1, h - 1, w, h).unwrap()] as usize + node.cost;
+                println!("poggers: {}", score);
+                return;
+            }
+            if parent.contains_key(&neighbour) {
+                continue;
+            }
+            if let Some(i) = index(neighbour[0], neighbour[1], w, h) {
+                let new_cost = cave[i] as usize + node.cost;
+                parent.insert(neighbour, node.pos);
+                nodes.push(Node {
+                    pos: neighbour,
+                    cost: new_cost,
+                });
+            }
+        }
+    }
+
+    unreachable!()
+}
+
 fn main() {
     let mut buffer = String::with_capacity(1024);
     let mut cave = Vec::with_capacity(10000);
@@ -49,34 +81,28 @@ fn main() {
         buffer.clear();
     }
 
-    let mut parent = HashMap::new();
-    let mut nodes = BinaryHeap::new();
-    nodes.push(Node {
-        pos: [0, 0],
-        cost: 0,
-    });
+    // part1
+    solve(&cave, w, h);
 
-    let finish = [w - 1, h - 1];
-    while let Some(node) = nodes.pop() {
-        for neighbour in neighbours(node.pos[0], node.pos[1]).into_iter() {
-            if neighbour == finish {
-                let score = cave[index(w - 1, h - 1, w, h).unwrap()] as usize + node.cost;
-                println!("p1: {}", score);
-                return;
-            }
-            if parent.contains_key(&neighbour) {
-                continue;
-            }
-            if let Some(i) = index(neighbour[0], neighbour[1], w, h) {
-                let new_cost = cave[i] as usize + node.cost;
-                parent.insert(neighbour, node.pos);
-                nodes.push(Node {
-                    pos: neighbour,
-                    cost: new_cost,
-                });
+    let w2 = w * 5;
+    let h2 = h * 5;
+    let mut cave2 = vec![0; w2 as usize * h2 as usize];
+    for y in 0..h {
+        for x in 0..w {
+            let cost = cave[index(x, y, w, h).unwrap()];
+
+            for j in 0..5 {
+                for i in 0..5 {
+                    let mut c = cost + (i as u8 + j as u8);
+                    while c > 9 {
+                        c -= 9;
+                    }
+                    cave2[index(x + i * w, y + j * h, w2, h2).unwrap()] = c;
+                }
             }
         }
     }
 
-    unreachable!()
+    // part2
+    solve(&cave2, w2, h2);
 }
