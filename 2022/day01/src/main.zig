effa6c0d5fc4b9e5756c5851d4142179f2b63046
Daniel Kiss <littlesnorrboy@gmail.com>
2022-12-01 10:52:04 +0100

    2022 day1 part2

--- 2022/day01/src/main.zig
+++ 2022/day01/src/main.zig
@@ -1,13 +1,16 @@
 const std = @import("std");
 
-pub fn part1(input: []const u8) anyerror!i32 {
-    var max_calories: i32 = 0;
+pub fn solve(input: []const u8) anyerror![3]i32 {
+    var max_calories: [3]i32 = [_]i32{ 0, 0, 0 };
     var current_calories: i32 = 0;
     var it = std.mem.split(u8, input, "\n");
     while (it.next()) |line| {
         if (line.len == 0) {
-            if (max_calories < current_calories) {
-                max_calories = current_calories;
+            for (max_calories) |*v, i| {
+                if (v.* < current_calories) {
+                    std.mem.rotate(i32, max_calories[0 .. i + 1], i);
+                    v.* = current_calories;
+                }
             }
             current_calories = 0;
         } else {
@@ -26,9 +29,14 @@ pub fn main() anyerror!void {
 
     var input = try file.readToEndAlloc(a, 32000);
 
-    var p1 = try part1(input);
+    var result = try solve(input);
 
-    std.log.info("part1: {}", .{p1});
+    std.log.info("part1: {}", .{result[2]});
+    var sum: i32 = 0;
+    for (result) |n| {
+        sum += n;
+    }
+    std.log.info("part2: {}", .{sum});
 }
 
 test "part1" {
@@ -49,7 +57,34 @@ test "part1" {
         \\10000
     ;
 
-    var result = try part1(input);
+    var result = try solve(input);
     var expected: i32 = 24000;
-    try std.testing.expectEqual(expected, result);
+    try std.testing.expectEqual(expected, result[2]);
+}
+
+test "part2" {
+    const input =
+        \\1000
+        \\2000
+        \\3000
+        \\
+        \\4000
+        \\
+        \\5000
+        \\6000
+        \\
+        \\7000
+        \\8000
+        \\9000
+        \\
+        \\10000
+    ;
+
+    var result = try solve(input);
+    var sum: i32 = 0;
+    for (result) |n| {
+        sum += n;
+    }
+    var expected: i32 = 45000;
+    try std.testing.expectEqual(expected, sum);
 }
