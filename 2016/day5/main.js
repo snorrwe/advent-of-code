50c626e7039b3f8b09e7d9e0f7ce1f19e80c7e22
Daniel Kiss <littlesnorrboy@gmail.com>
2022-12-01 14:56:13 +0100

    2016 day5 part2

--- 2016/day5/main.js
+++ 2016/day5/main.js
@@ -1,20 +1,35 @@
-const md5 = require('md5');
+const md5 = require("md5");
 
-function solve(base){
-    let i=0;
-    let result = [];
-    while(result.length < 8){
-        const input = base + i;
-        const hash = md5(input);
-        if (hash.startsWith('00000')) {
-            result.push(hash[5]);
-        }
-        i += 1;
+function solve(base) {
+  let i = 0;
+  let p1 = [];
+  let p2 = [
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+  ];
+  while (p2.some((x) => x === undefined)) {
+    const input = base + i;
+    const hash = md5(input);
+    if (hash.startsWith("00000")) {
+      let idx = hash[5];
+
+      if (p1.length < 8) p1.push(idx);
+
+      idx = parseInt(idx);
+      if (!isNaN(idx) && idx < 8 && !p2[idx]) {
+        p2[idx] = hash[6];
+      }
     }
-    return result.join('');
-
-
+    i += 1;
+  }
+  return [p1.join(""), p2.join("")].join("  ");
 }
 
-console.log(solve('abc'))
-console.log(solve('cxdnnyjw'))
+console.log(solve("abc"));
+console.log(solve("cxdnnyjw"));
