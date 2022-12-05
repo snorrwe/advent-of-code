59f7b7154f15ffe1207aa39ff669a6f7ca140e34
Daniel Kiss <littlesnorrboy@gmail.com>
2022-12-05 22:40:21 +0100

    2022 day5 part2

--- 2022/day05/src/main.rs
+++ 2022/day05/src/main.rs
@@ -1,13 +1,8 @@
-use std::collections::VecDeque;
+use std::{collections::VecDeque, str::Lines};
 
 use regex::Regex;
 
-fn solve(input: &str) -> (String, ()) {
-    let mut stacks = Vec::new();
-    stacks.reserve(10);
-
-    let mut lines = input.lines();
-
+fn parse_crates<'a>(lines: &mut Lines<'a>, stacks: &mut Vec<VecDeque<&'a str>>) {
     let re = Regex::new(r"\[([A-Z])\]|( {3})").unwrap();
     while let Some(line) = lines.next() {
         if line.len() == 0 {
@@ -25,6 +20,14 @@ fn solve(input: &str) -> (String, ()) {
             }
         }
     }
+}
+
+fn part1(input: &str) -> String {
+    let mut stacks = Vec::new();
+    stacks.reserve(10);
+
+    let mut lines = input.lines();
+    parse_crates(&mut lines, &mut stacks);
 
     let re = Regex::new(r"move (\d+) from (\d+) to (\d+)").unwrap();
     for line in lines {
@@ -40,23 +43,54 @@ fn solve(input: &str) -> (String, ()) {
             stacks[to - 1].push_back(x);
         }
     }
-    let part1 = stacks
+    stacks
         .into_iter()
         .map(|mut stack| stack.pop_back().unwrap_or(" "))
-        .collect();
-    (part1, ())
+        .collect()
+}
+
+fn part2(input: &str) -> String {
+    let mut stacks = Vec::new();
+    stacks.reserve(10);
+
+    let mut lines = input.lines();
+    parse_crates(&mut lines, &mut stacks);
+
+    let re = Regex::new(r"move (\d+) from (\d+) to (\d+)").unwrap();
+    let mut buffer = Vec::with_capacity(128);
+    for line in lines {
+        let Some(captures) = re.captures(line) else {
+            continue;
+        };
+        let n: usize = captures.get(1).unwrap().as_str().parse().unwrap();
+        let from: usize = captures.get(2).unwrap().as_str().parse().unwrap();
+        let to: usize = captures.get(3).unwrap().as_str().parse().unwrap();
+
+        for _ in 0..n {
+            let x = stacks[from - 1].pop_back().unwrap();
+            buffer.push(x);
+        }
+        for x in buffer.drain(..).rev() {
+            stacks[to - 1].push_back(x);
+        }
+    }
+    stacks
+        .into_iter()
+        .map(|mut stack| stack.pop_back().unwrap_or(" "))
+        .collect()
 }
 
 fn main() {
     let input = std::fs::read_to_string("input.txt").unwrap();
-    let (p1, _p2) = solve(&input);
+    let p1 = part1(&input);
     println!("p1: {}", p1);
-    // println!("p2: {}", p2);
+    let p2 = part2(&input);
+    println!("p2: {}", p2);
 }
 
 #[test]
-fn basic_test() {
-    let (part1, _part2) = solve(
+fn p1_test() {
+    let part1 = part1(
         r#"    [D]    
 [N] [C]    
 [Z] [M] [P]
@@ -69,4 +103,21 @@ move 1 from 1 to 2"#,
     );
 
     assert_eq!(part1, "CMZ")
+}
+
+#[test]
+fn p2_test() {
+    let part2 = part2(
+        r#"    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2"#,
+    );
+
+    assert_eq!(part2, "MCD");
 }
