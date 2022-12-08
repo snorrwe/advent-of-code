f8047bdb996d3911db3586de3a0423dfefc43714
Daniel Kiss <littlesnorrboy@gmail.com>
2022-12-08 17:22:10 +0100

    2022 day8 part2

--- 2022/day08/src/main.rs
+++ 2022/day08/src/main.rs
@@ -72,11 +72,65 @@ fn part1(grid: &Grid) -> usize {
     result
 }
 
+fn part2(grid: &Grid) -> usize {
+    let rows = grid.len();
+    let columns = grid[0].len();
+
+    let mut result = 0;
+    // for each inner point
+    for y in 1..rows - 1 {
+        for x in 1..columns - 1 {
+            let mut current_score = [0; 4];
+            let val = grid[y][x];
+            // scan left
+            for x in (0..x).rev() {
+                current_score[0] += 1;
+                if grid[y][x] >= val {
+                    break;
+                }
+            }
+
+            // scan right
+            for x in x + 1..rows {
+                current_score[1] += 1;
+                if grid[y][x] >= val {
+                    break;
+                }
+            }
+
+            // scan up
+            for y in (0..y).rev() {
+                current_score[2] += 1;
+                if grid[y][x] >= val {
+                    break;
+                }
+            }
+
+            // scan down
+            for y in y + 1..rows {
+                current_score[3] += 1;
+                if grid[y][x] >= val {
+                    break;
+                }
+            }
+
+            let current_score: usize = current_score.into_iter().product();
+            if current_score > result {
+                result = current_score;
+            }
+        }
+    }
+
+    result
+}
+
 fn main() {
     let inp = std::fs::read_to_string("input.txt").unwrap();
     let grid = parse(&inp);
     let p1 = part1(&grid);
     println!("part1: {p1}");
+    let p2 = part2(&grid);
+    println!("part2: {p2}");
 }
 
 #[test]
@@ -92,4 +146,19 @@ fn part1_test() {
     let res = part1(&grid);
 
     assert_eq!(21, res);
+}
+
+#[test]
+fn part2_test() {
+    let grid = parse(
+        r#"30373
+25512
+65332
+33549
+35390"#,
+    );
+
+    let res = part2(&grid);
+
+    assert_eq!(8, res);
 }
