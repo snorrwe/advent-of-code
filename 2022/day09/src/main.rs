766e4d3095e0f267ae079b31fc09580592364aee
Daniel Kiss <littlesnorrboy@gmail.com>
2022-12-09 13:30:27 +0100

    2022 day9 part2

--- 2022/day09/src/main.rs
+++ 2022/day09/src/main.rs
@@ -1,4 +1,5 @@
 use std::collections::HashSet;
+use utils::IVec2;
 
 fn cheby_dist(a: [i32; 2], b: [i32; 2]) -> i32 {
     (a[0] - b[0]).abs().max((a[1] - b[1]).abs())
@@ -34,12 +35,50 @@ fn part1(input: &str) -> usize {
     visited.len()
 }
 
+fn part2(input: &str) -> usize {
+    let mut visited = HashSet::new();
+    let mut rope = [IVec2::ZERO; 10];
+    visited.insert(IVec2::ZERO);
+
+    for line in input.lines() {
+        let mut line = line.split(" ");
+        let dir = line.next().unwrap();
+        let amount = line.next().unwrap().parse::<i32>().unwrap();
+        let delta: IVec2 = match dir {
+            "U" => IVec2::Y,
+            "D" => -IVec2::Y,
+            "L" => -IVec2::X,
+            "R" => IVec2::X,
+            _ => unreachable!(),
+        };
+
+        for _ in 0..amount {
+            let old_head = rope[0];
+            rope[0] = old_head + delta;
+
+            for i in 1..10 {
+                let new_head = rope[i - 1];
+                let tail = rope[i];
+                if new_head.chebyshev(tail) > 1 {
+                    let d = (new_head - tail).as_direction();
+                    rope[i] = tail + d;
+                } else {
+                    break;
+                }
+            }
+            visited.insert(rope[9]);
+        }
+    }
+    visited.len()
+}
+
 fn main() {
     let input = std::fs::read_to_string("input.txt").unwrap();
 
     let res = part1(&input);
-
     println!("part1: {res}");
+    let res = part2(&input);
+    println!("part2: {res}");
 }
 
 #[test]
@@ -56,4 +95,35 @@ R 2"#,
     );
 
     assert_eq!(13, res)
+}
+#[test]
+fn part2_simple_test() {
+    let res = part2(
+        r#"R 4
+U 4
+L 3
+D 1
+R 4
+D 1
+L 5
+R 2"#,
+    );
+
+    assert_eq!(1, res)
+}
+
+#[test]
+fn part2_test() {
+    let res = part2(
+        r#"R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20"#,
+    );
+
+    assert_eq!(36, res)
 }
