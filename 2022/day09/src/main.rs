9a49e4876de8a7ad1bfacd4d3cdbe775b7c42088
Daniel Kiss <littlesnorrboy@gmail.com>
2022-12-09 14:29:24 +0100

    2022 day9 refactor

--- 2022/day09/src/main.rs
+++ 2022/day09/src/main.rs
@@ -1,50 +1,14 @@
 use std::collections::HashSet;
 use utils::IVec2;
 
-fn cheby_dist(a: [i32; 2], b: [i32; 2]) -> i32 {
-    (a[0] - b[0]).abs().max((a[1] - b[1]).abs())
-}
-
-fn part1(input: &str) -> usize {
-    let mut visited = HashSet::new();
-    let mut head = [0; 2];
-    let mut tail = [0; 2];
-    visited.insert(tail);
+fn solve<const N: usize>(input: &str) -> usize {
+    debug_assert!(N >= 1);
+    let mut visited: HashSet<IVec2> = [IVec2::ZERO].into();
+    let mut rope = [IVec2::ZERO; N];
 
     for line in input.lines() {
         let mut line = line.split(" ");
-        let dir = line.next().unwrap();
-        let amount = line.next().unwrap().parse::<i32>().unwrap();
-        let delta = match dir {
-            "U" => [0, 1],
-            "D" => [0, -1],
-            "L" => [-1, 0],
-            "R" => [1, 0],
-            _ => unreachable!(),
-        };
-
-        for _ in 0..amount {
-            let new_head = [head[0] + delta[0], head[1] + delta[1]];
-            if cheby_dist(new_head, tail) > 1 {
-                tail = head;
-                visited.insert(tail);
-            }
-            head = new_head;
-        }
-    }
-    visited.len()
-}
-
-fn part2(input: &str) -> usize {
-    let mut visited = HashSet::new();
-    let mut rope = [IVec2::ZERO; 10];
-    visited.insert(IVec2::ZERO);
-
-    for line in input.lines() {
-        let mut line = line.split(" ");
-        let dir = line.next().unwrap();
-        let amount = line.next().unwrap().parse::<i32>().unwrap();
-        let delta: IVec2 = match dir {
+        let delta = match line.next().unwrap() {
             "U" => IVec2::Y,
             "D" => -IVec2::Y,
             "L" => -IVec2::X,
@@ -52,21 +16,19 @@ fn part2(input: &str) -> usize {
             _ => unreachable!(),
         };
 
-        for _ in 0..amount {
+        for _ in 0..line.next().unwrap().parse::<i32>().unwrap() {
             let old_head = rope[0];
             rope[0] = old_head + delta;
 
-            for i in 1..10 {
-                let new_head = rope[i - 1];
-                let tail = rope[i];
-                if new_head.chebyshev(tail) > 1 {
-                    let d = (new_head - tail).as_direction();
-                    rope[i] = tail + d;
+            for i in 1..N {
+                if rope[i - 1].chebyshev(rope[i]) > 1 {
+                    let d = (rope[i - 1] - rope[i]).as_direction();
+                    rope[i] = rope[i] + d;
                 } else {
-                    break;
+                    break; // not necessary just an optimization
                 }
             }
-            visited.insert(rope[9]);
+            visited.insert(rope[N - 1]);
         }
     }
     visited.len()
@@ -75,15 +37,15 @@ fn part2(input: &str) -> usize {
 fn main() {
     let input = std::fs::read_to_string("input.txt").unwrap();
 
-    let res = part1(&input);
+    let res = solve::<2>(&input);
     println!("part1: {res}");
-    let res = part2(&input);
+    let res = solve::<10>(&input);
     println!("part2: {res}");
 }
 
 #[test]
 fn part1_test() {
-    let res = part1(
+    let res = solve::<2>(
         r#"R 4
 U 4
 L 3
@@ -98,7 +60,7 @@ R 2"#,
 }
 #[test]
 fn part2_simple_test() {
-    let res = part2(
+    let res = solve::<10>(
         r#"R 4
 U 4
 L 3
@@ -114,7 +76,7 @@ R 2"#,
 
 #[test]
 fn part2_test() {
-    let res = part2(
+    let res = solve::<10>(
         r#"R 5
 U 8
 L 8
