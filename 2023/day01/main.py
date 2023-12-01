e8f6cbf3d3dd3e407b39c454af89806ef9277575
Daniel Kiss <littlesnorrboy@gmail.com>
2023-12-01 10:06:19 +0100

    2023 day01 part2

--- 2023/day01/main.py
+++ 2023/day01/main.py
@@ -26,3 +26,53 @@ for line in input.splitlines():
     total += 10 * first + last
 
 print(total)
+
+digits = [
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+]
+
+if len(sys.argv) > 1:
+    with open(sys.argv[1]) as f:
+        input = f.read()
+else:
+    input = """two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+"""
+
+
+
+total = 0
+for line in input.splitlines():
+    first = 0
+    last = 0
+    for i, c in enumerate(line):
+        try:
+            c = int(c)
+            if not first:
+                first = c
+            last = c
+        except:
+            for j, digit in enumerate(digits):
+                if line[i:].startswith(digit):
+                    c = j
+                    if not first:
+                        first = c
+                    last = c
+                    break
+    total += 10 * first + last
+
+print(total)
