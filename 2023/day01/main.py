e1005bd472333db123f7cd4676a533e74edb0bfd
Daniel Kiss <littlesnorrboy@gmail.com>
2023-12-01 10:26:10 +0100

    Slightly shorter

--- 2023/day01/main.py
+++ 2023/day01/main.py
@@ -3,29 +3,22 @@ import sys
 
 if len(sys.argv) > 1:
     with open(sys.argv[1]) as f:
-        input = f.read()
+        input1 = f.read()
+        input2 = input1
 else:
-    input = """1abc2
+    input1 = """1abc2
 pqr3stu8vwx
 a1b2c3d4e5f
 treb7uchet
 """
-
-total = 0
-for line in input.splitlines():
-    first = 0
-    last = 0
-    for c in line:
-        try:
-            c = int(c)
-            if not first:
-                first = c
-            last = c
-        except:
-            pass
-    total += 10 * first + last
-
-print(total)
+    input2 = """two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+"""
 
 digits = [
     "zero",
@@ -40,39 +33,31 @@ digits = [
     "nine",
 ]
 
-if len(sys.argv) > 1:
-    with open(sys.argv[1]) as f:
-        input = f.read()
-else:
-    input = """two1nine
-eightwothree
-abcone2threexyz
-xtwone3four
-4nineeightseven2
-zoneight234
-7pqrstsixteen
-"""
-
-
-
-total = 0
-for line in input.splitlines():
-    first = 0
-    last = 0
+total1 = 0
+total2 = 0
+for line in input1.splitlines():
+    first1 = 0
+    last1 = 0
+    first2 = 0
+    last2 = 0
     for i, c in enumerate(line):
         try:
             c = int(c)
-            if not first:
-                first = c
-            last = c
+            if not first1:
+                first1 = c
+                if not first2:
+                    first2 = c
+            last2 = last1 = c
         except:
             for j, digit in enumerate(digits):
                 if line[i:].startswith(digit):
                     c = j
-                    if not first:
-                        first = c
-                    last = c
+                    if not first2:
+                        first2 = c
+                    last2 = c
                     break
-    total += 10 * first + last
+    total1 += 10 * first1 + last1
+    total2 += 10 * first2 + last2
 
-print(total)
+print(total1)
+print(total2)
