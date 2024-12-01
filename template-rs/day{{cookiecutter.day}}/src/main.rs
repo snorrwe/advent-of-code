aec5f61f9354adef96a01e7e5425703ed87bccf1
Daniel Kiss <littlesnorrboy@gmail.com>
2024-12-01 08:27:31 +0100

    Add parse stub for the rust template

--- template-rs/day{{cookiecutter.day}}/src/main.rs
+++ template-rs/day{{cookiecutter.day}}/src/main.rs
@@ -1,8 +1,13 @@
+fn parse(input: String) -> String {
+    input
+}
+
 fn main() {
     let input = std::fs::read_to_string("input.txt").unwrap();
+    let input = parse(input);
 
-    println!("{}", part1(input.as_str()));
-    println!("{}", part2(input.as_str()));
+    println!("{}", part1(&input));
+    println!("{}", part2(&input));
 }
 
 fn part1(input: &str) -> i32 {
