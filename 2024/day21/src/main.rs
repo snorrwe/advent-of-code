5d71197084aac6d0c08e4c94d0f0fadf121db495
Daniel Kiss <littlesnorrboy@gmail.com>
2024-12-22 15:25:27 +0100

    imperative

--- 2024/day21/src/main.rs
+++ 2024/day21/src/main.rs
@@ -47,34 +47,32 @@ fn resolve_directional(c: u8) -> IVec2 {
 }
 
 fn plan_button_press(from: IVec2, to: IVec2, gap: IVec2, plan: &mut Vec<u8>) -> bool {
-    if from == to {
-        plan.push(b'A');
-        return true;
-    }
-    if from == gap {
-        return false;
-    }
     let d = to - from;
 
-    if d.x != 0 {
-        let horizontal = if d.x < 0 { b'<' } else { b'>' };
-        plan.push(horizontal);
-        if plan_button_press(from + IVec2::new(d.x / d.x.abs(), 0), to, gap, plan) {
-            return true;
-        } else {
-            plan.pop();
+    let dx = IVec2::new(d.x, 0);
+    let dy = IVec2::new(0, d.y);
+
+    let mut delta = [dx, dy];
+    if from.y == gap.y {
+        delta.swap(0, 1);
+    }
+
+    for d in delta {
+        if d.x != 0 {
+            let horizontal = if d.x < 0 { b'<' } else { b'>' };
+            for _ in 0..d.x.abs() {
+                plan.push(horizontal);
+            }
+        } else if d.y != 0 {
+            let vertical = if d.y < 0 { b'^' } else { b'v' };
+            for _ in 0..d.y.abs() {
+                plan.push(vertical);
+            }
         }
     }
-    if d.y != 0 {
-        let vertical = if d.y < 0 { b'^' } else { b'v' };
-        plan.push(vertical);
-        if plan_button_press(from + IVec2::new(0, d.y / d.y.abs()), to, gap, plan) {
-            return true;
-        } else {
-            plan.pop();
-        }
-    }
-    false
+    plan.push(b'A');
+
+    true
 }
 
 fn numeric_path(seq: &[u8]) -> Vec<u8> {
